516b5f85a5d2c4abf1a4381f1d79d780aeeddeec
rohit-447 <rohitkumar826344@gmail.com>
2025-02-13 02:04:53 +0530

    fixed checkbox

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -36,13 +36,13 @@ export default function ContactUs() {
             <div><h1 className='text-xl px-4 py-2'>Confirm Email</h1>
             <input type="email" placeholder="Email" className="border p-3 w-2/5 rounded-3xl" /></div>
             
-            <div className="grid grid-cols-2 gap-2  w-1/2">
-              <label><input type="checkbox" className="mr-2" /> Website Design</label>
-              <label><input type="checkbox" className="mr-2" /> Content Creation</label>
-              <label><input type="checkbox" className="mr-2" /> UX Design</label>
-              <label><input type="checkbox" className="mr-2" /> Strategy & Consulting</label>
-              <label><input type="checkbox" className="mr-2" /> User Research</label>
-              <label><input type="checkbox" className="mr-2" /> Other</label>
+            <div className="grid grid-cols-2 gap-2  w-2/3">
+              <label><input type="checkbox" className="mr-2 whitespace-nowrap accent-red-500" /> Website Design</label>
+              <label><input type="checkbox" className="mr-2 whitespace-nowrap accent-red-500" /> Content Creation</label>
+              <label><input type="checkbox" className="mr-2 whitespace-nowrap accent-red-500" /> UX Design</label>
+              <label><input type="checkbox" className="mr-2 whitespace-nowrap accent-red-500" /> Strategy & Consulting</label>
+              <label><input type="checkbox" className="mr-2 whitespace-nowrap accent-red-500" /> User Research</label>
+              <label><input type="checkbox" className="mr-2 whitespace-nowrap accent-red-500" /> Other</label>
             </div>
 
             <button type="submit" className="bg-black text-white px-6 py-3 rounded-xl w-full sm:w-auto">
